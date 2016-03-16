1d303814ce99aa6f733875306996d722c3003ffc
Evan Cobb <cobb.evan@gmail.com>
2016-03-16 01:04:45 +0100

    Adding CLI arguments for port and host for deployment

--- api/server.py
+++ api/server.py
@@ -1,4 +1,5 @@
 from bottle import run, route, debug, template, get, post
+import argparse
 
 @get('/data')
 def list():
@@ -9,4 +10,9 @@ def save_new():
     print("Got something")
     return {'message': 'got it'}
 
-run()
+parser = argparse.ArgumentParser()
+parser.add_argument('--port', type=int, default="5000")
+parser.add_argument('--host', default="127.0.0.1")
+args = parser.parse_args()
+
+run(port=args.port, host=args.host)
