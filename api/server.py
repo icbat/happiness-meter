eee5407301c9cdd0e15e2342fbe76a03739cc762
Evan Cobb <cobb.evan@gmail.com>
2016-03-16 20:18:20 -0500

    logging what we get posted to happiness-data

--- api/server.py
+++ api/server.py
@@ -1,4 +1,4 @@
-from bottle import run, route, debug, template, get, post
+from bottle import run, route, debug, template, get, post, request
 import argparse
 
 @get('/happiness-data')
@@ -7,12 +7,34 @@ def list():
 
 @post('/happiness-data')
 def save_new():
-    print("Got something")
+    print ("### Start transmission ###")
+    print_value ("request", request)
+    print_value ("request.json", request.json)
+    print_map (request.POST)
+    print_map (request.POST.files)
+    print_map (request.POST.forms)
+    print_map (request.forms)
+    print ("### End transmission ###")
+    print("\n\n")
+
     return {'message': 'got it'}
+
+def print_value(name, value):
+    print ("## " + str(name))
+    print (value)
+    print ("\n")
+
+def print_map(name, map):
+    print ("## " + str(name))
+    print ("Found a dictionary")
+    for key in map:
+        print("  -   " + str(key) + " : " + str(map[key]))
+    print ("\n")
+
 
 @post('/users/link')
 def link_users():
-    return {'message': 'yup, linkin'}
+    return {'message': 'not-yet-implemented, use /happiness-data for testing'}
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--port', type=int, default="5000")
