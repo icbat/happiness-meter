b9b182ed853ed2edd54b6e4e260f1554d9d392e9
Evan Cobb <cobb.evan@gmail.com>
2016-03-16 22:21:06 -0500

    returning back the JSON we receive on post

--- api/server.py
+++ api/server.py
@@ -1,4 +1,4 @@
-from bottle import Bottle
+from bottle import Bottle, request
 import argparse
 from bottle.ext.mongo import MongoPlugin
 from bson.json_util import dumps
@@ -19,18 +19,14 @@ def list(mongodb):
 
 @app.post('/happiness-data')
 def save_new():
-    print ("### Start transmission ###")
-    print_value ("request", request)
-    print_value ("request.json", request.json)
-    print_map ("request.POST", request.POST)
-    print_map ("request.POST.files", request.POST.files)
-    print_map ("request.POST.forms", request.POST.forms)
-    print_map ("request.forms", request.forms)
-    print_value ("key's value", request.json['key'])
-    print ("### End transmission ###")
-    print("\n\n")
+    data_point = request.json
+    if data_point is None:
+        print ("No JSON posted")
+        return {"message": "this endpoint expects JSON, try again!"}
 
-    return {'message': 'got it'}
+    print ("JSON received:")
+    print (dumps(data_point))
+    return dumps(data_point)
 
 @app.get('/users')
 def list():
@@ -39,18 +35,6 @@ def list():
 @app.post('/users/link')
 def link_users():
     return {'message': 'not-yet-implemented, use /happiness-data for testing'}
-
-def print_value(name, value):
-    print ("## " + str(name))
-    print (value)
-    print ("\n")
-
-def print_map(name, map):
-    print ("## " + str(name))
-    print ("Found a dictionary")
-    for key in map:
-        print("  -   " + str(key) + " : " + str(map[key]))
-    print ("\n")
 
 
 parser = argparse.ArgumentParser()
