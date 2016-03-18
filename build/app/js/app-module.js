57b444a7c4273c649b3b0b0321e59c2b408fff8a
Evan Cobb <cobb.evan@gmail.com>
2016-03-18 05:46:28 -0500

    trying with pre-built app

---
+++ build/app/js/app-module.js
@@ -0,0 +1,17 @@
+(function () {
+  'use strict';
+
+  /* @ngdoc object
+   * @name mot
+   * @description
+   *
+   */
+  angular
+    .module('mot', [
+      'ui.router',
+      'mgcrea.ngStrap',
+      'home'
+    ]);
+}());
+
+//# sourceMappingURL=app-module.js.map
--- start-server.sh
+++ start-server.sh
@@ -1,12 +1,8 @@
-echo "### Installing bower via NPM ###"
-npm install -g bower
-echo "### Installing gulp via NPM ###"
-npm install gulp
-echo "### Installing NPM dependencies ###"
-npm install
-echo "### Installing Bower dependencies ###"
-bower install
-echo "### Gulp Build ###"
-gulp build
+# echo "### Installing bower via NPM ###"
+# npm install -g bower
+# echo "### Installing NPM dependencies ###"
+# npm install
+# echo "### Installing Bower dependencies ###"
+# bower install
 echo "### Starting server ###"
 python api/server.py --port $PORT --host 0.0.0.0
